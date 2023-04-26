8fcc060903a98d650ca37094ca0612d8efcc8179
Marko Cuk <marko.cuk.brains22@gmail.com>
2023-04-26 16:05:26 +0200

    first commit

---
+++ routes/items.js
@@ -0,0 +1,43 @@
+const {getItems, getItem} = require('../controllers/items')
+
+const Item = {
+    type: 'object',
+    properties: {
+        id: {type: 'string'},
+        name: {type: 'string'},
+        },
+}
+
+const getItemsOpts = {
+    schema: {
+        response: {
+            200: {
+                type: 'array',
+                items: Item,
+            },
+        },
+    },
+    handler: getItems, 
+    }
+
+
+const getItemOpts = {
+    schema: {
+        response: {
+            200: Item,
+            },
+        },
+        handler: getItem,
+    }
+
+function itemRoutes( fastify, options, done) { // done (callback)
+
+fastify.get('/items', getItemsOpts)
+
+
+fastify.get('/items/:id', getItemOpts)
+
+    done()
+} 
+
+module.exports = itemRoutes
---
+++ server.js
@@ -0,0 +1,15 @@
+const fastify = require('fastify')({logger:true})
+fastify.register(require('./routes/items'))
+
+// start server!
+const start = async () => {
+    try {
+        await fastify.listen({port: 5000})
+    } catch(err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+}
+start();
+
+
